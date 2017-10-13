fa0ff2d39c328e6ee773d4970159252e99e22d2b
Kory Overbay <kory.overbay@gmail.com>
2017-10-12 19:27:42 -0500

    Initial Add to queue function. Basic conditions.

--- Project1/Project1/Elevator.cpp
+++ Project1/Project1/Elevator.cpp
@@ -18,7 +18,52 @@ Elevator::~Elevator()
 }
 
 void Elevator::Add(requests req) {
+	//Function takes into consideration where in the queue the request needs to be based on where the elevator is
+	//as well as where the request is coming from
+	//O(n) worst case time complexity
 
+	vector<requests>::iterator iter;
+	if (dir == 1) {					//If elevator is moving in up direction
+		if (req.curr < *curr) {			//If the new request's current floor is less than the elevator's current floor
+			for (iter = upQ.end(); iter != upQ.begin(); iter--) {		//Iterate backwards through the up 'queue'
+				if (iter->curr > *curr)						//If the request in the queue's current floor is > the elevator's current floor
+					upQ.push_back(req);				//Put the new request at the end of the Queue
+				else if (iter->curr < req.curr) {			//If the request in the queue's current floor is < the new request's current floor
+					upQ.insert(--iter, req);		//Insert the new request after the iterator's request
+					break;
+				}
+			}
+		}
+		else {			//If the new request's current floor is greater than the elevator's current floor
+			for (iter = upQ.begin(); iter != upQ.end(); iter++) {		//Iterate forward through the queue
+				if (iter->curr > req.curr) {		//If the request in the queue's current floor is greater than the new request's current floor
+					upQ.insert(iter, req);		//Insert the new request before the iterator's request
+					break;
+				}
+			}		//The logic for adding to the down queue is relatively similar, but with different conditions
+		}			//Since it considers floors in the oposite manner as the up queue
+	}
+	else {
+		if (req.curr > *curr) {
+			for (iter = downQ.end(); iter != downQ.begin(); iter--) {
+				if (iter->curr < *curr)
+					downQ.push_back(req);
+				else if (iter->curr > req.curr) {
+					downQ.insert(--iter, req);
+					break;
+				}
+			}
+		}
+		else {
+			for (iter = downQ.begin(); iter != downQ.end(); iter++) {
+				if (iter->curr < req.curr) {
+					downQ.insert(iter, req);
+					break;
+				}
+			}
+		}
+	}
+	
 }
 
 void Elevator::Gtfo()
