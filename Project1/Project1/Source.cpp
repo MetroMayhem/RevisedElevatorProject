b344fe4d5696a876526a0244e171d188d015557a
Alec Shern <asqk4@mail.umkc.edu>
2017-10-13 19:23:08 -0500

    Added a constructor to Requests for easy creation, added a source file.

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,13 @@
+#include "Elevator.h"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int main() {
+
+	Elevator elevator;
+	
+
+}
--- Project1/Project1/requests.h
+++ Project1/Project1/requests.h
@@ -3,7 +3,11 @@
 struct requests {
 	int curr;
 	int dest;
-	bool direct;
+	bool direction;
 	bool pickedUp;
+
+	requests(int _curr, int _dest, bool _direction, bool _pickedUp) :
+		curr(_curr), dest(_dest), direction(_direction), pickedUp(_pickedUp) {}
+
 
 };
