dcc3c119b67d8d4d834d1dfec3f81143342fb4f1
Alec Shern <asqk4@mail.umkc.edu>
2017-10-15 21:27:39 -0500

    Added constructor to requests.h

--- Project1/Project1/requests.h
+++ Project1/Project1/requests.h
@@ -4,16 +4,16 @@ struct requests {
 	int curr;
 	int dest;
 	bool direction;
-	bool pickedUp;
+
+	requests() : curr(0), dest(0), direction(0) {}
 
 	requests(int _curr, int _dest, bool _direction, bool _pickedUp) :
-		curr(_curr), dest(_dest), direction(_direction), pickedUp(_pickedUp) {}
+		curr(_curr), dest(_dest), direction(_direction) {}
 
 	 friend std::ostream& operator<<(std::ostream& cout, requests _req) {
 		cout << "Current floor: " << _req.curr << std::endl;
 		cout << "Destination floor: " << _req.dest << std::endl;
 		cout << "Direction desired: " << _req.direction << std::endl;
-		cout << "Picked up status: " << _req.pickedUp << std::endl;
 		cout << std::endl;
 		return cout;
 	}
